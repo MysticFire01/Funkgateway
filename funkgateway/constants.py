20cb0697b5c770db6d9b2014bf0e2d73fe9e0b97
MysticFire01 <114339309+MysticFire01@users.noreply.github.com>
2026-09-26 01:25:04 +0200

    FunkGateway UI 0.5.9.21: bundled default WAV directory

--- funkgateway/constants.py
+++ funkgateway/constants.py
@@ -5,7 +5,7 @@ Keeping paths in one place makes the rest of the source easier to read.
 from pathlib import Path
 
 APP_NAME = "FunkGateway UI"
-VERSION = "0.5.9.20"
+VERSION = "0.5.9.21"
 CFG_DIR = Path.home() / ".config" / "funkgateway-ui"
 CFG_FILE = CFG_DIR / "config.json"
 LOG_FILE = CFG_DIR / "gateway.log"
@@ -13,6 +13,7 @@ CW_FILE = CFG_DIR / "cw-id.wav"
 DTMF_FILE = CFG_DIR / "dtmf.wav"
 ID_RECORDING_FILE = CFG_DIR / "rufzeichen.wav"
 ROGER_FILE = CFG_DIR / "rogerbeep.wav"
+DEFAULT_WAV_DIR = Path(__file__).resolve().parents[1] / "default_wavs"
 
 
 def ensure_cfg():
