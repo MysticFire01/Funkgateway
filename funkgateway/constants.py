b265c0f56e68d46c8589cc332436c6f44938adce
MysticFire01 <114339309+MysticFire01@users.noreply.github.com>
2026-09-26 03:00:55 +0200

    FunkGateway UI 0.5.9.25: bump version

--- funkgateway/constants.py
+++ funkgateway/constants.py
@@ -5,7 +5,7 @@ Keeping paths in one place makes the rest of the source easier to read.
 from pathlib import Path
 
 APP_NAME = "FunkGateway UI"
-VERSION = "0.5.9.22"
+VERSION = "0.5.9.25"
 CFG_DIR = Path.home() / ".config" / "funkgateway-ui"
 CFG_FILE = CFG_DIR / "config.json"
 LOG_FILE = CFG_DIR / "gateway.log"
